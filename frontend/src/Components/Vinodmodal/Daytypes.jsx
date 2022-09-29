246036b296cbb00d27b3720428f3781fedd9a199
vin9012d <vin9012c@gmail.com>
2022-09-29 09:40:13 +0530

    day selection component added

--- frontend/src/Components/Vinodmodal/Daytypes.jsx
+++ frontend/src/Components/Vinodmodal/Daytypes.jsx
@@ -1,20 +1,23 @@
-import { Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
+import { Button, Menu, MenuButton, MenuItem, MenuList, Select } from '@chakra-ui/react'
+import { IoIosArrowDown } from "react-icons/io"
 import React from 'react'
+import { useState } from 'react'
 
 export const Daytypes = () => {
+    const [time, setTime] = useState("Week")
+    const handleSelect = (e) => {
+        console.log(e.target.value, 'value')
+        setTime(e.target.value)
+    }
   return (
-    <Menu>
-  {({ isOpen }) => (
-    <>
-      <MenuButton isActive={isOpen} as={Button} rightIcon={<ChevronDownIcon />}>
-        {isOpen ? 'Close' : 'Open'}
-      </MenuButton>
-      <MenuList>
-        <MenuItem>Download</MenuItem>
-        <MenuItem onClick={() => alert('Kagebunshin')}>Create a Copy</MenuItem>
-      </MenuList>
-    </>
-  )}
-</Menu>
+<Select value={time} onChange={handleSelect} placeholder={time}>
+  <option value='Week'>Semimonth</option>
+  <option value='Semimonth'>Semimonth</option>
+  <option value='Month'>Month</option>
+  <option value='Quarter'>Quarter</option>
+  <option value='Year'>Year</option>
+  <option value='Alltime'>Alltime</option>
+  <option value='Custom'>Custom</option>
+</Select>
   )
 }
