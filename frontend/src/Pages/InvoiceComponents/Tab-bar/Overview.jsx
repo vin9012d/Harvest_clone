d4640ebf15ada70a650cc1b107add9e9506d9640
ranajit das <dasranajit410@gmail.com>
2022-09-29 23:57:23 +0530

    Initial added of invoice page

---
+++ frontend/src/Pages/InvoiceComponents/Tab-bar/Overview.jsx
@@ -0,0 +1,46 @@
+import {React,useDisclosure} from "react";
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Box,
+    Button,
+    Text
+  } from '@chakra-ui/react'
+
+const Overview = () => {
+    // const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <div>
+      {/* <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>erjgsdjf</Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal> */}
+
+      <Box>
+
+      </Box>
+    </div>
+  );
+};
+
+export default Overview;
